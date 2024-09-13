dcaef48542e38f5f3ea34fcbb6e057c178f1f51b
Chainstrument <minitutoriel@gmail.com>
2024-09-13 19:57:03 +0200

    more shorted

--- map.js
+++ map.js
@@ -10,9 +10,6 @@ let result = ['20', '30', '40', '50', '60', '70'];
 
 // manipulate map function
 
-res = result.map(el => {
-    //console.log(el, key, arr);
-     return el * el
-});
+res = result.map(el =>  el * el);
 
 console.log(res);
