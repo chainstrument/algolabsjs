3ebe2a224d9a037931c04ae859a8788ebb6672da
Chainstrument <minitutoriel@gmail.com>
2024-09-13 19:46:55 +0200

    add map handle

---
+++ map.js
@@ -0,0 +1,17 @@
+let fruits = [ 'a', 'b', 'c', 'd', 'e', 'f'];
+
+
+fruits.map((fruit) => {
+    console.log(fruit);
+});
+
+
+let result = ['20', '30', '40', '50', '60', '70'];
+
+// manipulate map function
+res = result.map((result) => {
+     return result * 2
+});
+
+
+console.log(res);
