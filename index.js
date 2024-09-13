610f7c0efb9759d69ab11781e16b4ad7f6cdc111
Chainstrument <minitutoriel@gmail.com>
2024-09-13 10:29:37 +0200

    show obj attr

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const fruits = ["apple", "banana", "orange"];
 for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
+
+const person = { name: "John Doe", age: 30 };
+
+console.log(person.name);
